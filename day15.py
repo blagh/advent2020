031567b5929da09666574a4e0c9781befaa8ff47
Hannele Kormano <hkormano@pagerduty.com>
2020-12-15 10:00:30 -0500

    tiny tidy-up

--- day15.py
+++ day15.py
@@ -1,23 +1,24 @@
 from collections import defaultdict
 
 starting = [None, 8,13,1,0,18,9]
-seen = {starting[i]: [i] for i in range(len(starting))}
+seen = {starting[i]: i for i in range(len(starting))}
 print(seen)
 
+max_n = 30_000_000
 n = len(starting)
 last_number = 0
-while n <= 30_000_000:
-    print(f"{int(n / 30_000_000 * 100)}%: {last_number}")
+while n <= max_n:
+    print(f"{int(n / max_n * 100)}%: {last_number}")
 
     if seen.get(last_number) is None:
         # print(f"it's a new number: {last_number}")
-        seen[last_number] = [n]
+        seen[last_number] = n
         last_number = 0
     else:
         # print(f"got last numbers: {last_number} {seen[last_number]}")
 
-        next_number = n - seen[last_number][-1]
-        seen[last_number] = [n]
+        next_number = n - seen[last_number]
+        seen[last_number] = n
 
         last_number = next_number
 
