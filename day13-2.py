1d957c96a99ae3342c9fe709830c982977d9a4eb
Hannele Kormano <hkormano@pagerduty.com>
2020-12-15 00:02:41 -0500

    bah

--- day13-2.py
+++ day13-2.py
@@ -2,15 +2,51 @@ from math import floor, lcm
 # note: lcm requires python 3.9
 
 def bus_lcm(buses):
-
+    multiple = 1
     length = len(buses)
-    new_buses = []
-    for i in range(len(buses)):
-        if buses[i]:
-            new_buses.append(buses[i] + length - i - 1)
+    print(length, buses)
 
-    print(buses, new_buses)
-    return lcm(*new_buses) - length
+    while True:
+        print(f"{buses[0]}: trying multiple: {multiple} {multiple * buses[0]}")
+
+        it_worked = True
+        first_bus = buses[0] * multiple
+        first_offset = length - 1
+        for i in range(1, length):
+            offset = length - i - 1
+            this_bus = buses[i]
+
+            # no constraints ~~~
+            if this_bus == None:
+                # print(f"{i}: skipping None")
+                continue
+
+            # if this_bus is exactly what we want, let's take it
+            if this_bus + offset == first_bus + first_offset:
+                # print(f"{i}: no multiplier needed: {this_bus}:{this_bus + offset} {first_bus}:{first_bus + first_offset}")
+                continue
+
+            # if this_bus is greater than next bus + 1, then we can't make it smaller
+            if this_bus + offset > first_bus + first_offset:
+                # print(f"{i}: this_bus is too big: {this_bus + offset} {first_bus + first_offset}")
+                it_worked = False
+                break
+
+            # check if we can make a multiple + offset work
+            multiplier = int((first_bus + first_offset) / this_bus)
+            if this_bus * multiplier + offset == first_bus + first_offset:
+                # print(f"{i}: this_bus with a mult: {this_bus * multiplier}:{this_bus * multiplier + offset} {first_bus}:{first_bus + first_offset}")
+                continue
+
+            # print(f"{i}: didn't work {this_bus * multiplier}:{this_bus * multiplier + offset} {first_bus}:{first_bus + first_offset}")
+            it_worked = False
+            break
+
+        if it_worked:
+            print(f"Success! {first_bus}")
+            return first_bus
+
+        multiple += 1
 
 cases = [
     ([17,None,13,19], 3417),
@@ -27,7 +63,10 @@ for (buses, expected) in cases:
         print("not yet")
         print(expected, actual)
         break
+    else:
+        print("success!!!")
 
 
 ## FINAL BUSS
-# buses = [13,None,None,41,None,None,None,None,None,None,None,None,None,569,None,29,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,19,None,None,None,23,None,None,None,None,None,None,None,937,None,None,None,None,None,37,None,None,None,None,None,None,None,None,None,None,17]
+final_buss = [13,None,None,41,None,None,None,None,None,None,None,None,None,569,None,29,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,19,None,None,None,23,None,None,None,None,None,None,None,937,None,None,None,None,None,37,None,None,None,None,None,None,None,None,None,None,17]
+bus_lcm(final_buss)
