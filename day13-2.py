7df4ad2a5be18a6139ad30ab1fbb5b7a7aedb3b6
Hannele Kormano <hkormano@pagerduty.com>
2020-12-14 23:02:03 -0500

    this felt so clever and i even installed a whole new version of python for it but it didn't work

--- day13-2.py
+++ day13-2.py
@@ -1,28 +1,32 @@
-from math import floor
+from math import floor, lcm
+# note: lcm requires python 3.9
 
-def lcm(buses):
-    pass
+def bus_lcm(buses):
 
+    length = len(buses)
+    new_buses = []
+    for i in range(len(buses)):
+        if buses[i]:
+            new_buses.append(buses[i] + length - i - 1)
 
-
-diffs = {}
-for bus in buses:
-    if bus:
-        min = floor(departure / bus)
-
-        print(bus, departure, departure / bus, min, bus * min, departure - bus * min)
-        diffs[bus] = bus - (departure - bus * min)
+    print(buses, new_buses)
+    return lcm(*new_buses) - length
 
 cases = [
-    ([7,13,None,None,59,None,31,19], 1068781),
     ([17,None,13,19], 3417),
+    ([7,13,None,None,59,None,31,19], 1068781),
     ([67,7,59,61], 754018),
     ([67,None,7,59,61], 779210),
     ([67,7,None,59,61], 1261476),
     ([1789,37,47,1889], 1202161486)
 ]
 
-for (list, expected) in cases:
+for (buses, expected) in cases:
+    actual = bus_lcm(buses)
+    if expected != actual:
+        print("not yet")
+        print(expected, actual)
+        break
 
 
 ## FINAL BUSS
