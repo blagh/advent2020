78231022bc72433dac3bd7906d77b2c3830bc4d2
Hannele Kormano <hkormano@pagerduty.com>
2020-12-20 14:21:25 -0500

    day 10 complete ~~

--- day10.py
+++ day10.py
@@ -5,13 +5,7 @@ with open("day10input.txt") as file:
 
 lines = [int(l[:-1]) for l in lines]
 lines = sorted(lines)
-lines = lines + [lines[-1] + 3]
-
-
-
-def compatible(n, lines):
-    return list(filter(lambda x: x > n and x <= n+3, lines))
-nodes = {l: compatible(l, lines) for l in lines}
+lines = [0] + lines + [lines[-1] + 3]
 
 def part_1(lines):
 
@@ -28,4 +22,34 @@ def part_1(lines):
     print(one_diffs, three_diffs)
 
 part_1(lines)
+
+
+def compatible(n, lines):
+    return list(filter(lambda x: x > n and x <= n+3, lines))
+
+
+sequences = {}
+
+def count_sequences(n, nodes, depth=0):
+    if n in sequences:
+        return sequences[n]
+
+    children = nodes[n]
+
+    if len(children) == 0:
+        return 1
+
+    if len(children) == 1:
+        count = count_sequences(children[0], nodes, depth)
+        sequences[n] = count
+
+    total = 0
+    for c in children:
+        total += count_sequences(c, nodes, depth+1)
+    print(" "*depth + f"recurse many: {total}")
+    sequences[n] = total
+    return total
+
+nodes = {l: compatible(l, lines) for l in lines}
 print(nodes)
+print(count_sequences(0, nodes))
