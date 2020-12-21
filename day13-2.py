8a32751d14c6d23e0023f737f9d5a4993938b978
Hannele Kormano <hkormano@pagerduty.com>
2020-12-20 22:16:36 -0500

    Credit where due

--- day13-2.py
+++ day13-2.py
@@ -1,52 +1,42 @@
+from functools import reduce
 from math import floor, lcm
-# note: lcm requires python 3.9
+
+# sauce: https://fangya.medium.com/chinese-remainder-theorem-with-python-a483de81fbb8
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+
+    for n_i, a_i in zip(n,a):
+        p = prod // n_i # for large p
+        sum += a_i * mul_inv(p, n_i) * p
+
+    return sum, prod, sum % prod
+
+def mul_inv(a, b):
+    if b == 1: return 1
+
+    b0 = b
+    x0, x1 = 0,1
+    while a > 1:
+        q = a // b
+        (a, b) = (b, a % b)
+        x0, x1 = x1 - q * x0, x0
+
+    if x1 < 0: x1 += b0
+    return x1
 
 def bus_lcm(buses):
-    multiple = 1
-    length = len(buses)
-    print(length, buses)
+    n = []
+    a = []
+    for i in range(len(buses)):
+        if buses[i]:
+            n.append(buses[i])
+            a.append(i)
 
-    while True:
-        print(f"{buses[0]}: trying multiple: {multiple} {multiple * buses[0]}")
 
-        it_worked = True
-        first_bus = buses[0] * multiple
-        first_offset = length - 1
-        for i in range(1, length):
-            offset = length - i - 1
-            this_bus = buses[i]
-
-            # no constraints ~~~
-            if this_bus == None:
-                # print(f"{i}: skipping None")
-                continue
-
-            # if this_bus is exactly what we want, let's take it
-            if this_bus + offset == first_bus + first_offset:
-                # print(f"{i}: no multiplier needed: {this_bus}:{this_bus + offset} {first_bus}:{first_bus + first_offset}")
-                continue
-
-            # if this_bus is greater than next bus + 1, then we can't make it smaller
-            if this_bus + offset > first_bus + first_offset:
-                # print(f"{i}: this_bus is too big: {this_bus + offset} {first_bus + first_offset}")
-                it_worked = False
-                break
-
-            # check if we can make a multiple + offset work
-            multiplier = int((first_bus + first_offset) / this_bus)
-            if this_bus * multiplier + offset == first_bus + first_offset:
-                # print(f"{i}: this_bus with a mult: {this_bus * multiplier}:{this_bus * multiplier + offset} {first_bus}:{first_bus + first_offset}")
-                continue
-
-            # print(f"{i}: didn't work {this_bus * multiplier}:{this_bus * multiplier + offset} {first_bus}:{first_bus + first_offset}")
-            it_worked = False
-            break
-
-        if it_worked:
-            print(f"Success! {first_bus}")
-            return first_bus
-
-        multiple += 1
+    print(n, a)
+    (sum, prod, mod_prod) = chinese_remainder(n, a)
+    return prod - mod_prod
 
 cases = [
     ([17,None,13,19], 3417),
@@ -69,4 +59,4 @@ for (buses, expected) in cases:
 
 ## FINAL BUSS
 final_buss = [13,None,None,41,None,None,None,None,None,None,None,None,None,569,None,29,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,19,None,None,None,23,None,None,None,None,None,None,None,937,None,None,None,None,None,37,None,None,None,None,None,None,None,None,None,None,17]
-bus_lcm(final_buss)
+print(bus_lcm(final_buss))
