072ee75fc1aa058d7fc7500acd0d9614b6c68870
Hannele Kormano <hkormano@pagerduty.com>
2020-12-28 16:34:10 -0500

    Day 19 complete ~~ oh god finally ~

--- day19.py
+++ day19.py
@@ -11,9 +11,13 @@ class OptionRule:
         self.options = [FollowRule(o) for o in options]
 
     def _get_stash(self, key):
+        if type(key) == list:
+            key = "'".join(key)
         return STASH[repr(self)].get(key)
 
     def _set_stash(self, key, value):
+        if type(key) == list:
+            key = "'".join(key)
         STASH[repr(self)][key] = value
 
     # def generate_messages(self, rules, depth):
@@ -23,10 +27,14 @@ class OptionRule:
     #     return messages
 
     def matches(self, message, depth):
-        options = list(filter(lambda o: len(o) <= len(message), self.options))
-        if len(options) == 0:
-            print(TAB*depth, "OPTION FAIL - NO OPTIONS", message)
-            return False, message
+        options = self.options
+        if type(message) != list:
+            options = list(filter(lambda o: len(o) <= len(message), self.options))
+            if len(options) == 0:
+                print(TAB*depth, "OPTION FAIL - TOO SHORT", message, self.options)
+                return False, ""
+        else:
+            message = list(filter(lambda m: len(m) > 0, message))
 
         if result := self._get_stash(message):
             print(TAB*depth, "OPTION STASH", message, result)
@@ -52,8 +60,8 @@ class OptionRule:
             print(TAB*depth, "OPTION MULTI SUCCESS", matches)
             return self._return(message, True, matches)
 
-        print(TAB*depth, "OPTION FAIL", message)
-        return self._return(message, False, message)
+        print(TAB*depth, "OPTION FAIL", self.options, message)
+        return self._return(message, False, "")
 
     def _return(self, original_message, success, message):
         self._set_stash(original_message, (success, message))
@@ -67,43 +75,37 @@ class FollowRule:
         self.follows = [ReferenceRule(f) for f in follows]
 
     def _get_stash(self, key):
+        if type(key) == list:
+            key = "|".join(key)
+
         return STASH[repr(self)].get(key)
 
     def _set_stash(self, key, value):
+        if type(key) == list:
+            key = "|".join(key)
+
         STASH[repr(self)][key] = value
 
-    # def generate_messages(self, rules, depth):
-    #     messages = [""]
-    #     print(TAB*depth, "FOLLOW", self.follows)
-    #
-    #     for f in self.follows:
-    #         print(TAB*depth, f)
-    #         sub = f.generate_messages(rules, depth+1)
-    #         messages = ["".join(a) for a in list(product(messages, sub))]
-    #
-    #     print(TAB*depth, "FOLLOW", messages)
-    #     return messages
-
     def matches(self, message, depth):
+        if type(message) == list:
+            message = list(filter(lambda m: len(m) >= len(self.follows), message))
+        elif len(message) < len(self.follows):
+            print(TAB*depth, "FOLLOW FAILURE -- TOO SHORT!", original_message, self.follows)
+            return False, ""
+
         original_message = message
         if self._get_stash(message):
             print(TAB*depth, "FOLLOW STASH", self._get_stash(message))
             return self._get_stash(message)
 
-        if len(message) < len(self.follows):
-            print(TAB*depth, "FOLLOW FAILURE -- TOO SHORT!", original_message)
-            return False, original_message
-
         print(TAB*depth, "FOLLOW", message, self.follows)
         for f in self.follows:
-            # print(TAB*depth, f)
-
             (did_match, message) = f.matches(message, depth+1)
             if not did_match:
-                print(TAB*depth, "FOLLOW FAILURE", original_message)
-                return self._return(False, original_message, original_message)
+                print(TAB*depth, "FOLLOW FAILURE", self.follows, original_message)
+                return self._return(False, original_message, "")
 
-        print(TAB*depth, "FOLLOW SUCCESS", message)
+        print(TAB*depth, "FOLLOW SUCCESS", self.follows, message)
         return self._return(True, original_message, message)
 
     def _return(self, success, original, message):
@@ -121,19 +123,23 @@ class ReferenceRule:
         self.rule_no = rule_no
 
     def _get_stash(self, key):
+        if type(key) == list:
+            key = "r".join(key)
+
         return STASH[repr(self)].get(key)
 
     def _set_stash(self, key, value):
+        if type(key) == list:
+            key = "r".join(key)
+
         STASH[repr(self)][key] = value
 
     def matches(self, message, depth):
         if result := self._get_stash(message):
-            print(TAB*depth, "REF STASH", message, result)
+            print(TAB*depth, "REF STASH", self.rule_no, message, result)
             return result
 
-        print(TAB*depth, "REF", message, self.rule_no)
-        matches = RULES[self.rule_no].matches(message, depth+1)
-        print(TAB*depth, "REF", matches)
+        matches = RULES[self.rule_no].matches(message, depth)
         return self._return(message, *matches)
 
     def _return(self, original_message, success, message):
@@ -148,32 +154,34 @@ class Rule:
         self.value = value
 
     def matches(self, message, depth):
-        print(TAB*depth, "RULE", message, self.value)
-
         if len(message) == 0:
-            print(TAB*depth, "RULE FAIL :/ NO MOAR MESSAGE")
+            print(TAB*depth, f"RULE {self.value} FAIL :/ NO MOAR MESSAGE")
             return False, ""
 
         if type(message) == list:
+            print(TAB*depth, "RULE", message, self.value)
             sub_matches = []
             for m in message:
                 if m and m[0] == self.value:
                     sub_matches.append(m[1:])
 
             if len(sub_matches) == 1:
+                print(TAB*depth, "RULE SUCCESS!", sub_matches[0])
                 return True, sub_matches[0]
 
             if len(sub_matches) > 0:
+                print(TAB*depth, "RULE SUCCESS!", sub_matches)
                 return True, sub_matches
 
-            return False, message
+            print(TAB*depth, "RULE FAIL :/", self.value, message)
+            return False, ""
 
         if message[0] == self.value:
             print(TAB*depth, "RULE SUCCESS!", message[0], message[1:])
             return True, message[1:]
 
-        print(TAB*depth, "RULE FAIL :/", message)
-        return False, message
+        print(TAB*depth, "RULE FAIL :/", self.value, message)
+        return False, ""
 
     def __repr__(self):
         return repr(self.value)
@@ -201,6 +209,7 @@ for r in sorted(RULES.keys()):
 
 # valid_messages = rules[0].generate_messages(rules, 0)
 # print(valid_messages)
+# aaabbbbbbaaaabaababaabababbabaaabbababababaaa
 
 messages = []
 with open("day19input.txt") as file:
@@ -210,8 +219,10 @@ actuals = []
 for message in messages:
     print("---", message, "---")
     (did_match, match) = RULES[0].matches(message, 0)
+    if type(match) == list:
+        match = "" if "" in match else "multi!"
 
-    if did_match:
+    if did_match and len(match) == 0:
         print("--- MATCH!", message, "---")
         actuals.append(message)
     else:
