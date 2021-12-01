0b84995aafe771c80410e8d81f0d70c36bccdcb8
Hannele Kormano <hkormano@pagerduty.com>
2021-12-01 17:41:50 -0500

    Day 1 complete!

--- 2021/day01.py
+++ 2021/day01.py
@@ -13,3 +13,18 @@ for x in numbers:
     prev_x = x
 
 print(inc_count)
+
+# prev_sum = 199 + 200 + 208
+prev_sum = 141 + 152 + 164
+inc_count = 0
+for i in range(len(numbers)):
+
+    new_sum = sum(numbers[i:i+3])
+
+    print(prev_sum, numbers[i:i+3], new_sum)
+
+    if new_sum > prev_sum:
+        inc_count += 1
+    prev_sum = new_sum
+
+print(inc_count)
