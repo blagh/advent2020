f09a4039454eee054293b877ac275544ea52ab62
Hannele Kormano <hkormano@pagerduty.com>
2021-12-03 16:42:39 -0500

    moar generic

--- 2021/day03-2.py
+++ 2021/day03-2.py
@@ -9,29 +9,16 @@ line_len = len(lines[0])
 def count_ones(position, lines):
     return len(list(filter(lambda l: l[position] == "1", lines)))
 
-def filter_lines(req_char, position, lines):
+def filter_lines(position, one_char, zero_char, sensor, lines):
+    if len(lines) == 1:
+        return lines
+
+    one_count = count_ones(position, lines)
+    req_char = one_char if one_count >= len(lines) / 2 else zero_char
+
+    print(sensor, position, one_count, req_char)
+
     return list(filter(lambda l: l[position] == req_char, lines))
-
-def filter_o2_lines(position, lines):
-    if len(lines) == 1:
-        return lines
-
-    one_count = count_ones(position, lines)
-    req_char = "1" if one_count >= len(lines) / 2 else "0"
-
-    print("o2", position, one_count, req_char)
-    return filter_lines(req_char, position, lines)
-
-def filter_co2_lines(position, lines):
-    if len(lines) == 1:
-        return lines
-
-    one_count = count_ones(position, lines)
-    req_char = "0" if one_count >= len(lines) / 2 else "1"
-
-    print("co2", position, one_count, req_char)
-    return filter_lines(req_char, position, lines)
-
 
 o2_cands = lines
 co2_cands = lines
@@ -39,8 +26,8 @@ co2_cands = lines
 print(o2_cands, co2_cands)
 
 for i in range(line_len):
-    o2_cands = filter_o2_lines(i, o2_cands)
-    co2_cands = filter_co2_lines(i, co2_cands)
+    o2_cands = filter_lines(i, "1", "0", "o2", o2_cands)
+    co2_cands = filter_lines(i, "0", "1", "co2", co2_cands)
 
     print(o2_cands, co2_cands)
 
