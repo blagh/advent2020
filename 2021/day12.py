8970b9c97612769d051d362a726152928eb98338
Hannele Kormano <hkormano@pagerduty.com>
2021-12-26 15:06:53 -0500

    Day 12 complete ~~

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,13 +1,13 @@
 class Node:
     def __init__(self, name):
-        self.visited = False
+        self.visited = 0
         self.name = name
         self.neighbours = []
 
     def add_neighbour(self, node):
         self.neighbours.append(node)
 
-    def is_entrance(self):
+    def _is_entrance(self):
         return self.name == "start"
 
     def _is_exit(self):
@@ -16,26 +16,44 @@ class Node:
     def _is_big_cave(self):
         return self.name.isupper()
 
-    def _can_visit(self):
-        return self._is_big_cave() or not self.visited
+    def _can_visit(self, can_double_visit):
+        if self._is_big_cave():
+            return True
 
-    def visit(self):
-        if not self._can_visit():
+        if self._is_entrance() or not can_double_visit:
+            return self.visited < 1
+
+        return self.visited < 2
+
+    def visit(self, can_double_visit=True, depth=0):
+        if not self._can_visit(can_double_visit):
+            # print(' ' * depth, self.name, 'noope')
             return 0
 
         if self._is_exit():
+            print(' ' * depth, "exit!! +1")
             return 1
 
-        self.visited = True
+        print(' ' * depth, "--", self.name, ":", self.visited, "--")
+        pre_visit = self.visited
+
+        self.visited += 1
+        if not self._is_big_cave() and self.visited == 2:
+            print(' ' * depth, 'dubla!')
+            can_double_visit = False
+
         path_count = 0
         for node in self.neighbours:
-            path_count += node.visit()
+            path_count += node.visit(can_double_visit, depth + 1)
+        else:
+            pass
+            # print(' ' * depth, 'all done, counted', path_count)
 
-        self.visited = False
+        self.visited = pre_visit
         return path_count
 
     def __repr__(self):
-        return self.name + ": " + " ".join([n.name for n in self.neighbours])
+        return repr(self.visited) + " " + self.name + ": " + " ".join([n.name for n in self.neighbours])
 
 NODES = {}
 
