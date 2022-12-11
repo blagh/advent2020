2bfaa7918414e1ea2c21ec7dd7f170d036dc7548
Hannele Kormano <hkormano@pagerduty.com>
2022-12-11 01:16:38 -0500

    new new day

--- newday.py
+++ newday.py
@@ -1,10 +1,15 @@
+def run_test(input, expected, test_method):
+    actual = test_method(input)
+
+    print(input, "\nactual:", actual, "\nexpect:", expected)
+    assert(expected == actual)
+
 def run_tests(tests, test_method):
     for input, expected in tests:
-        output = test_method(input)
+        run_test(input, expected, test_method)
 
-        print(input, output, "expected", expected)
-        assert(output == expected)
-
+def print_2d_array(array):
+    print("\n".join(["".join(line) for line in array]), "\n\n\n")
 
 with open("input.txt") as file:
     for line in file.readlines():
